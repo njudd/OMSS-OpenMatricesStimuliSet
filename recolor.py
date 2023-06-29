6b149e8e215fa7cd709382c149c6153182dcdfe3
Nicholas Judd <nickkjudd@gmail.com>
2023-06-29 15:01:09 +0200

    added note

--- recolor.py
+++ recolor.py
@@ -13,6 +13,7 @@ simpleRPM_1.gimme()
 # code to make into a function
 whole_img = simpleRPM_1.ans_img
 #step 1 make multiply into colors values (three's)
+# THIS step is already in gimme()
 whole_img = np.repeat(whole_img[:, :, np.newaxis], 3, axis=2)
 
 # step 2; target and change the values
@@ -32,3 +33,9 @@ plt.show()
 # 84, (0, 188, 212)
 # 56, (56, 142, 60)
 # 28, (46, 204, 113)
+
+
+# so the function should take simpleRPM_1.ans_img and recode the colors
+
+
+
