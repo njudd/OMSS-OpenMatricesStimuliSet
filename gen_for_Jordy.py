0ba0e17831be255a0a3ab0d8b98abe018d90fba3
Nicholas Judd <nickkjudd@gmail.com>
2023-07-06 21:05:37 +0200

    made some more notes

--- gen_for_Jordy.py
+++ gen_for_Jordy.py
@@ -10,10 +10,15 @@ import numpy as np
 #raven_gen.attribute.COLOR_VALUES
 
 raven_gen.attribute.SIZE_VALUES = (.4, .65, .9)
+
+# maybe do 4? .25, .5, .75 1
+# idk what ever is sensible
+
+# also maybe less colors?
+
 raven_gen.attribute.SIZE_MAX = 2
 
-ruleset1 = Ruleset(size_rules=[RuleType.CONSTANT])
-#PROGRESSION
+ruleset1 = Ruleset(size_rules=[RuleType.PROGRESSION])
 
 
 ruleset = Ruleset(size_rules=[RuleType.CONSTANT],
@@ -22,9 +27,14 @@ ruleset = Ruleset(size_rules=[RuleType.CONSTANT],
                   number_rules=[RuleType.DISTRIBUTE_THREE],
                   position_rules=[RuleType.CONSTANT])
 
+# Where is the noise attribute can you chanage that?
+# with "rulset" it will just pick four rules at random; not ideal behaviour?
 
 #ruleset = Ruleset(size_rules=[RuleType.CONSTANT, RuleType.PROGRESSION], shape_rules=list(RuleType))
 rpm = Matrix.make(list(MatrixType)[2], ruleset=ruleset, n_alternatives=3)
+
+# I would fool around up the alternatives count;
+# The alternatives by definition don't follow the rule set; so you might have to figure out a way to make reasonable(ish) alternatives?
 
 #simpleRPM = Matrix.make(list(MatrixType)[2], n_alternatives = 0, ruleset=ruleset2)
 
