d2a7f37472c92bbe2096365261f6c5e62eca4bbc
Nicholas Judd <nickkjudd@gmail.com>
2023-08-25 19:25:31 +0200

    making the first round of examples

--- gen_for_Jordy.py
+++ gen_for_Jordy.py
@@ -9,38 +9,152 @@ import numpy as np
 #raven_gen.attribute.SIZE_VALUES
 #raven_gen.attribute.COLOR_VALUES
 
-raven_gen.attribute.SIZE_VALUES = (.4, .65, .9)
-
+raven_gen.attribute.SIZE_VALUES = (.3, .5, .7, .9)
+raven_gen.attribute.SIZE_MAX = 3 # need to tell it the length of the new vector when it changes
 # maybe do 4? .25, .5, .75 1
 # idk what ever is sensible
 
 # also maybe less colors?
 
-raven_gen.attribute.SIZE_MAX = 2
-
-ruleset1 = Ruleset(size_rules=[RuleType.PROGRESSION])
-
-
-ruleset = Ruleset(size_rules=[RuleType.CONSTANT],
-                  shape_rules=[RuleType.CONSTANT],
-                  color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.DISTRIBUTE_THREE],
-                  position_rules=[RuleType.CONSTANT])
-
 # Where is the noise attribute can you chanage that?
 # with "rulset" it will just pick four rules at random; not ideal behaviour?
 
 #ruleset = Ruleset(size_rules=[RuleType.CONSTANT, RuleType.PROGRESSION], shape_rules=list(RuleType))
-rpm = Matrix.make(list(MatrixType)[2], ruleset=ruleset, n_alternatives=3)
+
 
 # I would fool around up the alternatives count;
 # The alternatives by definition don't follow the rule set; so you might have to figure out a way to make reasonable(ish) alternatives?
 
 #simpleRPM = Matrix.make(list(MatrixType)[2], n_alternatives = 0, ruleset=ruleset2)
 
+
+# all constant
+ruleset_1 = Ruleset(size_rules=[RuleType.CONSTANT],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+
+# size distribute 3
+ruleset_2 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+
+# size ARITHMETIC
+ruleset_3 = Ruleset(size_rules=[RuleType.ARITHMETIC],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+# size PROGRESSION
+ruleset_4 = Ruleset(size_rules=[RuleType.PROGRESSION],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+
+# size distribute 3 plus color distribute 3
+ruleset_5 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.DISTRIBUTE_THREE],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+
+# color distribute 3 plus size progression
+ruleset_6 = Ruleset(size_rules=[RuleType.CONSTANT],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.DISTRIBUTE_THREE],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+# color PROGRESSION, position CONSTANT
+ruleset_7 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE],
+                  shape_rules=[RuleType.DISTRIBUTE_THREE],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+#shape progression, position CONSTANT
+ruleset_8 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE],
+                  shape_rules=[RuleType.PROGRESSION],
+                  color_rules=[RuleType.DISTRIBUTE_THREE],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+# size, shape and color as PROGRESSION
+ruleset_9 = Ruleset(size_rules=[RuleType.PROGRESSION],
+                  shape_rules=[RuleType.PROGRESSION],
+                  color_rules=[RuleType.PROGRESSION],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+# size & shape as ARITHMETIC while position progression
+ruleset_10 = Ruleset(size_rules=[RuleType.ARITHMETIC],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.ARITHMETIC],
+                  number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
+                  position_rules=[RuleType.CONSTANT])
+
+#
+rules = [ruleset_1, ruleset_2, ruleset_3, ruleset_4, ruleset_5, ruleset_6, ruleset_7, ruleset_8, ruleset_9, ruleset_10]
+
+
+# because its only single pieces (i.e., list(MatrixType)[0])
+# you can't change position
+
 os.getcwd()
-os.chdir('/Users/njudd/Desktop')
-rpm.save(".", "test")
+os.chdir('/Users/njudd/Desktop/rpms')
+for w in range(len(rules)):
+    os.mkdir("rpm_rule" + str(w))
+    os.chdir("rpm_rule" + str(w))
+    #print(w)
+    for i in range(5):
+        loopname = "rpm_ct_"
+        loopname += str(i)
+        #print("innerloop")
+        #print(i)
+        rpm = Matrix.make(list(MatrixType)[0], ruleset=rules[w], n_alternatives=3)
+        os.mkdir(loopname)  # making a dir for the rpm stuff
+        rpm.save(loopname + "/.", loopname)  # going in that dir, also naming the stimuli by the loopname
+
+        with open(loopname + "/" + loopname + "output.txt",
+                  "a") as f:  # going into the folder and making an output per item
+            print(rpm.rules, file=f)
+
+        with open("Global_output.txt", "a") as f:  # going into the folder and making an output per item
+            print(rpm.rules, file=f)
+    os.chdir("..")
+
+    # barf rules
+    with open("Global_rules.txt", "a") as f:  # going into the folder and making an output per item
+        print(rules[w], file=f)
+
+
+os.getcwd()
+
+
+os.chdir("/Users/njudd/Desktop/rpms_100rand")
+# just making 100 randomly sampled
+for i in range(100):
+    loopname = "rpm_ct_"
+    loopname += str(i)
+    # print("innerloop")
+    # print(i)
+    rpm = Matrix.make(list(MatrixType)[0], n_alternatives=3)
+    os.mkdir(loopname)  # making a dir for the rpm stuff
+    rpm.save(loopname + "/.", loopname)  # going in that dir, also naming the stimuli by the loopname
+
+    with open(loopname + "/" + loopname + "output.txt",
+              "a") as f:  # going into the folder and making an output per item
+        print(rpm.rules, file=f)
+
+    with open("Global_output.txt", "a") as f:  # going into the folder and making an output per item
+        print(rpm.rules, file=f)
+
+
+
+
+
+# maybe make a panda's dataframe of the rules as well?
+
 
 # only doing gimme so I can take a look at it without saving
 #rpm.gimme()
@@ -49,3 +163,12 @@ rpm.save(".", "test")
 #plt.show()
 print(rpm.rules)
 #print(rpm)
+
+with open("output.txt", "a") as f: # a is for append
+  print("start")
+  print(rpm.rules, file=f)
+  print("end")
+
+
+
+
