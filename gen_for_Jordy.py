1f165908a82b2d1969237870dabbb6d0502c2241
Nicholas Judd <nickkjudd@gmail.com>
2023-11-12 21:15:41 +0100

    working on 3 things 1) making a naming convention, 2) making the loop name each problem uniquely, 3) also making the layouts in the loop

--- gen_for_Jordy.py
+++ gen_for_Jordy.py
@@ -5,24 +5,21 @@ from PIL import Image
 import os
 import numpy as np
 
+#raven_gen.attribute.SIZE_VALUES; #raven_gen.attribute.COLOR_VALUES
+
+raven_gen.attribute.SIZE_VALUES = (.3, .5, .7, .9) # maybe do 4? .25, .5, .75 1
+raven_gen.attribute.SIZE_MAX = 3 # need to tell it the length of the new vector when it changes
+
+
 ### Naming of stimuli
 # The ordering matters; this is for saving and not naming because Py doesn’t like the dot
 #
 # R = rule; P = Problem
-# c = constant; p = progression; a = arithmetic; d = dist3
-# cl = color, sh = shape, sz = size, n = number, p = position,
-# a = all, r = rest
+## cl = color, sh = shape, sz = size, num = number, pos = position (all = all, r = rest)
+### C = constant; P = progression; A = arithmetic; D = dist3
 #
 # Example:
-# R4_a.sz_c.r_P1_alternative2 (rule 1, arithmetic size, constant rest, Problem 1, Alternative 2)
-
-
-
-#raven_gen.attribute.SIZE_VALUES
-#raven_gen.attribute.COLOR_VALUES
-
-raven_gen.attribute.SIZE_VALUES = (.3, .5, .7, .9) # maybe do 4? .25, .5, .75 1
-raven_gen.attribute.SIZE_MAX = 3 # need to tell it the length of the new vector when it changes
+# R4_aSZ_cR_P1_alternative2 (rule 1, arithmetic size, constant rest, Problem 1, Alternative 2)
 
 #### There is a google doc called "MatrixStimuliNotes" as well
 
@@ -34,6 +31,9 @@ raven_gen.attribute.SIZE_MAX = 3 # need to tell it the length of the new vector 
 #
 # with "rulset" it will just pick four rules at random; not ideal behaviour?
 # ^^^ this does not happen; the thing is 'number' and 'position' are linked they form 'configuration'
+
+# ^ this only happens with list(MatrixType)[0] on the others it lists "position" and "num"
+
 # while the others (color, sizzze, shape) are a different class (see the SI of paper)
 # therefore the function does not allow non-constant rules to occur on both of the attributes
 #
@@ -55,52 +55,62 @@ R1 = Ruleset(size_rules=[RuleType.CONSTANT],
                   number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
                   position_rules=[RuleType.CONSTANT])
 
-R2 = Ruleset(size_rules=[RuleType.PROGRESSION],
+R2 = Ruleset(size_rules=[RuleType.PROGRESSION], # R2_szP_rC
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
+                  number_rules=[RuleType.CONSTANT],
                   position_rules=[RuleType.CONSTANT])
 
-R3 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE],
+R3 = Ruleset(size_rules=[RuleType.DISTRIBUTE_THREE], # R3_szD_rC
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
+                  number_rules=[RuleType.CONSTANT],
                   position_rules=[RuleType.CONSTANT])
 
-R4 = Ruleset(size_rules=[RuleType.ARITHMETIC],
+R4 = Ruleset(size_rules=[RuleType.ARITHMETIC], # R4_szA_rC
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
+                  number_rules=[RuleType.CONSTANT],
                   position_rules=[RuleType.CONSTANT])
 
-R5 = Ruleset(size_rules=[RuleType.CONSTANT],
+R5 = Ruleset(size_rules=[RuleType.CONSTANT], # R5_shP_rC
                   shape_rules=[RuleType.PROGRESSION],
                   color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
+                  number_rules=[RuleType.CONSTANT],
                   position_rules=[RuleType.CONSTANT])
 
-R6 = Ruleset(size_rules=[RuleType.CONSTANT],
+R6 = Ruleset(size_rules=[RuleType.CONSTANT], # R6_shD_rC
                   shape_rules=[RuleType.DISTRIBUTE_THREE],
                   color_rules=[RuleType.CONSTANT],
-                  number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
+                  number_rules=[RuleType.CONSTANT],
                   position_rules=[RuleType.CONSTANT])
+
+
+
+
+######## are these okay...?
 
 # advanced for the other layouts
 
-R7 = Ruleset(size_rules=[RuleType.CONSTANT],
+R7_numP_rC = Ruleset(size_rules=[RuleType.CONSTANT],
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT], # you have too many color options & not enough blocks for this too work
                   number_rules=[RuleType.PROGRESSION], # if you only do single stimulis this isn't an issue
-                  position_rules=[RuleType.CONSTANT])
+                  position_rules=None)
 
 
-R8 = Ruleset(size_rules=[RuleType.CONSTANT],
+#####
+# these rules are problematic, I think because you are trying to change
+# number and position independently (which doesn't work)
+# could try making them both the same rule?
+
+R8_numD_rC = Ruleset(size_rules=[RuleType.CONSTANT],
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT], # you have too many color options & not enough blocks for this too work
                   number_rules=[RuleType.DISTRIBUTE_THREE], # if you only do single stimulis this isn't an issue
                   position_rules=[RuleType.CONSTANT])
 
-R9 = Ruleset(size_rules=[RuleType.CONSTANT],
+R9_numA_rC = Ruleset(size_rules=[RuleType.CONSTANT],
                   shape_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT], # you have too many color options & not enough blocks for this too work
                   number_rules=[RuleType.ARITHMETIC], # if you only do single stimulis this isn't an issue
@@ -117,6 +127,14 @@ R11 = Ruleset(size_rules=[RuleType.CONSTANT],
                   color_rules=[RuleType.CONSTANT], # you have too many color options & not enough blocks for this too work
                   number_rules=[RuleType.CONSTANT], # if you only do single stimulis this isn't an issue
                   position_rules=[RuleType.DISTRIBUTE_THREE])
+
+rct = Ruleset(size_rules=[RuleType.CONSTANT],
+                  shape_rules=[RuleType.CONSTANT],
+                  color_rules=[RuleType.CONSTANT],
+                  number_rules=[RuleType.DISTRIBUTE_THREE],
+                  position_rules=[RuleType.DISTRIBUTE_THREE])
+
+
 
 #### playspace ####
 # import raven_gen
@@ -149,7 +167,7 @@ R11 = Ruleset(size_rules=[RuleType.CONSTANT],
 #                      color_rules=[RuleType.CONSTANT]
 #                      )
 #
-# rpm_ct = Matrix.make(list(MatrixType)[1], ruleset=ruleset_ct) #, n_alternatives=5
+# rpm_ct = Matrix.make(list(MatrixType)[1], ruleset=rct) #, n_alternatives=5
 # rpm_ct.save(path = ".", puzzle_name="ct_rav")
 # print(rpm_ct.rules)
 # #print(rpm_ct.rules, file="rules.txt")
@@ -166,10 +184,11 @@ R11 = Ruleset(size_rules=[RuleType.CONSTANT],
 # first_val = list(rules.values())[0]
 
 # using dicts instead of lists
-rules = {'R1':R1,'R2':R2, 'R3':R3,'R4':R4, 'R5':R5, 'R6':R6}
+rules = {'R1_allC':R1,'R2_szP_rC':R2, 'R3_szD_rC':R3,
+         'R4_szA_rC':R4, 'R5_shP_rC':R5, 'R6_shD_rC':R6}
 
-rules_extra = {'R1':R1,'R2':R2, 'R3':R3,'R4':R4, 'R5':R5, 'R6':R6,
-               'R7':R7,'R8':R8, 'R9':R9}
+# rules_extra = {'R1':R1,'R2':R2, 'R3':R3,'R4':R4, 'R5':R5, 'R6':R6,
+#                'R7':R7,'R8':R8, 'R9':R9}
 
 ##### new rules
 # not sure if these are good
@@ -201,11 +220,64 @@ os.chdir('/Users/njudd/surfdrive/Shared/ravenStim/rpms_new_CT/layout1')
 
 # BIG THING MISSING IS THE NEW NAMING CONVENTION FOR RULES!!!!!!!
 
+layout_list = {"L1":0, "L2":1,"L3":2}
+
+os.chdir("/Users/njudd/Desktop/ct_ravGen/")
+
+
+# list(MatrixType)[layout_list[list(layout_list)[ll]]]
+# ^^^ this line of code is so disgusting you need to fix it
+# layout_index.values()(1)
+# TypeError: 'dict_values' object is not callable
+
+
+for ll in range(len(layout_list)): # ll = layout loop index
+    os.mkdir("Layout_" + list(layout_list)[ll])
+    os.chdir("Layout_" + list(layout_list)[ll])
+
+# now go over the vector of rules
+    for w in range(len(rules)):
+        os.mkdir("rpm_" + list(rules)[w])
+        os.chdir("rpm_" + list(rules)[w])
+        # print(w)
+        for i in range(3):
+            loopname = ("rpm_" + list(rules)[w])
+            loopname += ("P" + str(i + 1))  # plus one to get rid of Python indexing
+            # print("innerloop")
+            # print(i)
+            rpm = Matrix.make(list(MatrixType)[layout_list[list(layout_list)[ll]]], ruleset=list(rules.values())[w], n_alternatives=8)
+            os.mkdir(loopname)  # making a dir for the rpm stuff
+            probname = ("Layout1" + loopname)
+            rpm.save(loopname + "/.", probname)  # going in that dir, also naming the stimuli by the loopname
+
+            with open(loopname + "/" + probname + "output.txt",
+                      "a") as f:  # going into the folder and making an output per item
+                print(rpm.rules, file=f)
+
+            with open("Global_output.txt", "a") as f:  # going into the folder and making an output per item
+                print(rpm.rules, file=f)
+        os.chdir("..")
+
+        # barf rules
+        with open("Global_rules.txt", "a") as f:  # going into the folder and making an output per item
+            print(list(rules)[w], file=f)
+
+    os.chdir("..")
+
+
+
+# now you can make another one just for the rules you don't use in all layouts
+
+
+
+
+
+
 for w in range(len(rules)):
     os.mkdir("rpm_" + list(rules)[w])
     os.chdir("rpm_" + list(rules)[w])
     #print(w)
-    for i in range(10):
+    for i in range(3):
         loopname = ("rpm_" + list(rules)[w])
         loopname += ("P" + str(i+1)) # plus one to get rid of Python indexing
         #print("innerloop")
